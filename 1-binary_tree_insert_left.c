d1e3103d413c2c2a558a0ca803cd055606f3d315
Malek Mayeh <loukatankionline@gmail.com>
2022-09-03 11:25:00 +0100

    inserts a node as the left child

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -9,17 +9,12 @@
 
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
-	binary_tree_t *newnode = binary_tree_node(parent, value);
+	binary_tree_t *newnode;
 
-	if (parent == NULL)
-	{
-		return (NULL);
-	}
-	if (newnode == NULL)
-	{
-		return (NULL);
-	}
+	if (parent == NULL || newnode == NULL)
+	return (NULL);
 	
+	newnode = binary_tree_node(parent, value);
 
 	if (parent->left != NULL)
 	{
