534faaed41952da3a97a04e61dc9bd56634b4785
Malek Mayeh <loukatankionline@gmail.com>
2022-09-03 13:21:36 +0100

    goes through bin tree

--- 6-binary_tree_preorder.c
+++ 6-binary_tree_preorder.c
@@ -4,7 +4,7 @@
  * binary_tree_preorder - goes through a binary tree using pre-order traversal
  * @tree: pointer to the root node of the tree to traverse
  * @func: pointer to a function to call for each node
- * Return: 1 if node is a root, otherwise 0
+ * Return: nothing
  */
 
 void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
