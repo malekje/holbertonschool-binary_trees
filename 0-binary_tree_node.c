4df9bdbdd5a47821059f0dab614e64e1b963dd83
Malek Mayeh <loukatankionline@gmail.com>
2022-09-03 11:07:07 +0100

    creates a binary tree node

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -8,15 +8,15 @@
  */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-    binary_tree_t *newnode = malloc(sizeof(binary_tree_t));
+	binary_tree_t *newnode = malloc(sizeof(binary_tree_t));
 
-    if (newnode == NULL)
-        return (NULL);
+	if (newnode == NULL)
+	return (NULL);
 
-    newnode->n = value;
+	newnode->n = value;
 	newnode->parent = parent;
 	newnode->left = NULL;
 	newnode->right = NULL;
 
-    return (newnode);
+	return (newnode);
 }
