201aa5e3dc46267cfc8bb4e03277f1589c5929fa
MedAliKrifa <daliolimona@gmail.com>
2022-09-04 17:34:15 +0100

    9-binary_tree_height.c

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -7,15 +7,15 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-    if (tree == NULL)
-        return 0;
-    else
-    {
-        int lDepth = binary_tree_height(tree->left);
-        int rDepth = binary_tree_height(tree->right);
-        if (lDepth > rDepth)
-            return (lDepth + 1);
-        else
-            return (rDepth + 1);
-    }
+	if (tree == NULL)
+		return 0;
+	else
+	{
+		int lDepth = binary_tree_height(tree->left);
+		int rDepth = binary_tree_height(tree->right);
+		if (lDepth > rDepth)
+			return (lDepth + 1);
+		else
+			return (rDepth + 1);
+	}
 }
