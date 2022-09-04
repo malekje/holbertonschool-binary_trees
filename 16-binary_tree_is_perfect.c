2e60f091a8781ec06a8a849ccbf4be478fc9a302
MedAliKrifa <daliolimona@gmail.com>
2022-09-04 19:31:59 +0100

    16-binary_tree_is_perfect.c

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -30,7 +30,8 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 		return (0);
 	if (binary_tree_depth(tree->left) == binary_tree_depth(tree->right))
 	{
-		if (binary_tree_is_perfect(tree->left) && binary_tree_is_perfect(tree->right))
+		if (binary_tree_is_perfect(tree->left) &&
+			binary_tree_is_perfect(tree->right))
 			return (1);
 	}
 	return (0);
