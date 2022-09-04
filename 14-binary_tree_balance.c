70e68cf94b127115d27d8a92e62084029f8d8d8c
MedAliKrifa <daliolimona@gmail.com>
2022-09-04 19:00:32 +0100

    14-binary_tree_balance.c

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -10,8 +10,6 @@ size_t binary_tree_height(const binary_tree_t *tree)
 
 	if (tree == NULL)
 		return (0);
-	if (!tree->right && !tree->left)
-		return (0);
 	lDepth = binary_tree_height(tree->left);
 	rDepth = binary_tree_height(tree->right);
 
